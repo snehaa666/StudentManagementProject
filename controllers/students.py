f6bc1a2db3f263cc31b45dd8a5a36bca1a96d38e
psneha221 <psneha.reddy0221@gmail.com>
2025-11-29 10:12:50 +0000

    create and get students

--- controllers/students.py
+++ controllers/students.py
@@ -3,8 +3,19 @@ from core.responses import send_json, send_404
 from core.request import parse_json_body
 from services.student_services import (
     service_get_all
+    , service_get_one
+    , service_create
 )
 
 
 def get_all_students(handler):
     return send_json(handler,200, service_get_all())
+
+def get_student(handler, student_id):
+    student = service_get_one(student_id)
+    return send_json(handler, 200, student) if student else send_404(handler)
+
+def create_student(handler):
+    data = parse_json_body(handler)
+    new_student = service_create(data)
+    return send_json(handler, 201, new_student)
--- services/student_services.py
+++ services/student_services.py
@@ -1,6 +1,15 @@
 from database.queries import(
     db_get_all
+     , db_get_one
+    , db_create
+
 )
 
 def service_get_all():
     return db_get_all()
+
+def service_get_one(student_id):
+    return db_get_one(student_id)
+
+def service_create(data):
+    return db_create(data)
