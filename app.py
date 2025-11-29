be6b72f7ed74c13e869e7955e052d7ba1cbda879
psneha221 <psneha.reddy0221@gmail.com>
2025-11-29 06:11:40 +0000

    feat:add databse connecting

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+# Starts the API server and initializes the database
+
 from http.server import HTTPServer
 from router import StudentRouter
+from database.connection import init_database
 
-def run_server(port=8000):
-    
-    server = HTTPServer(("", port), StudentRouter)
-    print(f"🚀 Server running at http://localhost:{port}")
+def run_server():
+    init_database()
+    server = HTTPServer(("", 8000), StudentRouter)
+    print("🚀 Server running at http://localhost:8000")
     server.serve_forever()
 
 if __name__ == "__main__":
